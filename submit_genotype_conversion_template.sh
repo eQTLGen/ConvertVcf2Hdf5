0f7234f14c24d6ea41431ae6c0b793fbe7ad1b37
Urmo Võsa <urmo.vosa@gmail.com>
2022-02-03 14:37:41 +0200

    Update config files

--- submit_genotype_conversion_template.sh
+++ submit_genotype_conversion_template.sh
@@ -4,23 +4,29 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=5G
-#SBATCH --job-name="RunHASEwNextflow"
+#SBATCH --job-name="ConvertVcf2Hdf5"
 
-# Load needed system tools (Java 8 is required, one of singularity or anaconda - python 2.7 is needed,
-# depending on the method for dependancy management). The exact names of tool modules might depend on HPC.
+#These are needed modules in UT HPC to get singularity and Nextflow running. Replace with appropriate ones for your HPC.
 module load java-1.8.0_40
-module load python/2.7.15/native
 module load singularity/3.5.3
 module load squashfs/4.4
 
+# Define paths
 nextflow_path=[full path to your Nextflow executable]
 
+genopath=[Folder with input genotype files in .vcf.gz format]
+outputpath=[Folder where to write genotype data in h5 format]
+snplist=[path to the SNP ID list]
+studyname=[your study nameCohortName_ExpressionPlatformName]
+
+# Optional argument for the command
+# --samplelist '[Path to file with sample IDs]'
+
 NXF_VER=20.10.0 ${nextflow_path}/nextflow run ConvertVcf2Hdf5.nf \
---inputpath '[Folder with genotype files in .vcf format]' \
---outputpath '[Folder where to write genotype data in h5 format]' \
---snplist '[path to SNPList_MAF_0005.txt]' \
---samplelist '[Path to file with sample IDs]' \
---studyname '[CohortName_ExpressionPlatformName]' \
--profile [singularity,slurm] \
+--inputpath ${genopath} \
+--outputpath ${outputpath} \
+--snplist ${snplist} \
+--studyname ${studyname} \
+-profile slurm,singularity \
 -with-report GenotypeConversionReport.html \
 -resume
